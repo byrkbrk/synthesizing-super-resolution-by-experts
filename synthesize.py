a87ee1751b87b5e9aed45d9e113c9b7990f632a2
burak <byrkbrk@gmail.com>
2024-08-12 00:20:20 +0300

    update synthesize.py file

---
+++ synthesize.py
@@ -0,0 +1,24 @@
+from argparse import ArgumentParser
+from synthesizer import SRSynthesizer
+
+
+
+def parse_arguments():
+    """Returns parsed arguments"""
+    parser = ArgumentParser(description="Synthesize super resolution images (x4) by SeeMore")
+    parser.add_argument("image_name",
+                        type=str,
+                        default=None,
+                        help="""Name of the image that be upscaled. 
+                                Note image that be processed must be in `low-res-images` directory""")
+    parser.add_argument("--device",
+                        type=str,
+                        default=None,
+                        choices=["cuda", "mps", "cpu"],
+                        help="Name of the GPU device that be used during inference. Default: None")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    SRSynthesizer(device=args.device).synthesize(args.image_name)
